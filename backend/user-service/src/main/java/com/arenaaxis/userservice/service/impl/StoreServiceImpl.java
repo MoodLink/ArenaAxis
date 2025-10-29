dc0c65f928f3a2cfa556813c30a53c68b8946381
doan-zigexn <doannhb@zigexn.vn>
2025-10-29 22:16:43 +0700

    "Fix favorite"

--- backend/user-service/src/main/java/com/arenaaxis/userservice/service/impl/StoreServiceImpl.java
+++ backend/user-service/src/main/java/com/arenaaxis/userservice/service/impl/StoreServiceImpl.java
@@ -40,7 +40,6 @@ import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
