b10d9b3f4918082b8e39343b5acb6a2c6663623d
hoanghuu1207 <khronal777@gmail.com>
2025-11-27 00:31:49 +0700

    add cron update order statuspayment

---
+++ backend/order-service/src/cron/updateOrderStatus.js
@@ -0,0 +1,24 @@
+import cron from "node-cron";
+import { Order } from "../models/order.model.js";
+
+cron.schedule("*/10 * * * * *", async () => {
+  try {
+    const fiveMinutesAgo = new Date(Date.now() - 1 * 60 * 1000);
+
+    const result = await Order.updateMany(
+      {
+        statusPayment: "PENDING",
+        createdAt: { $lt: fiveMinutesAgo },
+      },
+      { $set: { statusPayment: "FAILED" } }
+    );
+
+    if (result.modifiedCount > 0) {
+      console.log(
+        `Đã cập nhật ${result.modifiedCount} đơn hàng hết hạn thành FAILED`
+      );
+    }
+  } catch (error) {
+    console.error("Cron job cập nhật FAILED gặp lỗi:", error);
+  }
+});
