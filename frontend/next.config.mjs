d4cd1d084f95ef3e720fd188f5e4079862ae2837
binhbo73 <phuongbinh732004@gmail.com>
2025-12-04 14:38:15 +0700

    change next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,8 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -15,7 +20,7 @@ const nextConfig = {
     pagesBufferLength: 5,
   },
   webpack(config) {
-    config.resolve.alias['@'] = require('path').resolve(__dirname);
+    config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
 }
